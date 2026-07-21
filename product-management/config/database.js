eeae6e018d237a7b5f8fd02758cb69599406d9df
Thanh Kka <thuyminet0@gmail.com>
2026-07-21 17:22:49 +0700

    git commit -m "Cài đặt Mongoose"

---
+++ product-management/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+module.exports.connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Kết nối Database thành công!");
+  } catch (error) {
+    console.error("Lỗi kết nối DB:", error);
+  }
+};
--- product-management/index.js
+++ product-management/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const router = require("./router/client/index.route");
 
+const database = require("./config/database");
+database.connect();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
-
 
 app.use(express.static("public"));
 router(app);
