262c527454c4f0d91f2ddb4af3e3ca547e4f8a1e
Thanh Kka <thuyminet0@gmail.com>
2026-07-21 15:50:01 +0700

    git commit -m "Cau hinh PUG"

--- product-management/controller/client/home.controller.js
+++ product-management/controller/client/home.controller.js
@@ -1,5 +1,6 @@
 module.exports.index = {
   index: (req, res) => {
     res.render("client/pages/home/index");
+    pageTitle: "Trang chu";
   }
 };
--- product-management/controller/client/product.controller.js
+++ product-management/controller/client/product.controller.js
@@ -1,3 +1,4 @@
 module.exports.index = (req, res) => {
   res.render("client/pages/products/index");
+  pageTitle: "Danh sách sản phẩm";
 };
