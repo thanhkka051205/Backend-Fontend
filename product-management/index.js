6f519e9bd454c9c7e9c0f581aa9117076c7e6bb4
Thanh Kka <thuyminet0@gmail.com>
2026-07-21 15:23:55 +0700

    git add.

--- product-management/index.js
+++ product-management/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const router = require("./router/client/index.route");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
