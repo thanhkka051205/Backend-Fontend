d3ffd0cbe7ae4a1b4ba58d92ec2fc9bc949c625d
Thanh Kka <thuyminet0@gmail.com>
2026-07-22 17:29:29 +0700

    3. Tính năng lọc theo trạng thái

--- product-management/controllers/admin/product.controller.js
+++ product-management/controllers/admin/product.controller.js
@@ -1,13 +1,49 @@
-const Product=require("../../models/product.model");
+const Product = require("../../models/product.model");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    deleted: false
-  });
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
 
-  console.log(products);
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status === req.query.status,
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status === "");
+    filterStatus[index].class = "active";
+  }
+
+  let find = {
+    deleted: false,
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
+
+  // 4. Render dữ liệu ra file Pug
   res.render("admin/pages/products/index", {
-    pageTitle: "Quan Ly San Pham",
-    products: products
+    pageTitle: "Quản Lý Sản Phẩm",
+    products: products,
+    filterStatus: filterStatus,
   });
 };
