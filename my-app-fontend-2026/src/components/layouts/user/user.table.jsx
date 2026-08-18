43eeeacbdf74812b129957906c2f8f9e4534d3f6
Thanh Kka <thuyminet0@gmail.com>
2026-08-18 21:36:13 +0700

    git commit -m"Hiển thị bảng Users"

--- my-app-fontend-2026/src/components/layouts/user/user.table.jsx
+++ my-app-fontend-2026/src/components/layouts/user/user.table.jsx
@@ -1,79 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Flex, Space, Table, Tag } from "antd";
+import { getAllUserAPI } from "../../../services/api.service";
 
 const UserTable = () => {
+  const [dataUsers, setDataUser] = useState([]);
+
+  const handleEditUser = () => {};
+
+  const handleDeleteUser = () => {};
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      render: (text) => <a>{text}</a>,
+      title: "ID",
+      dataIndex: "idUsers",
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "Họ và tên",
+      dataIndex: "fullName",
     },
     {
-      title: "Address",
-      dataIndex: "address",
-      key: "address",
-    },
-    {
-      title: "Tags",
-      key: "tags",
-      dataIndex: "tags",
-      render: (_, { tags }) => (
-        <Flex gap="small" align="center" wrap>
-          {tags.map((tag) => {
-            let color = tag.length > 5 ? "geekblue" : "green";
-            if (tag === "kawaii") {
-              color = "volcano";
-            }
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })}
-        </Flex>
-      ),
+      title: "Email",
+      dataIndex: "email",
     },
     {
       title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Space size="medium">
-          <a>Invite {record.name}</a>
-          <a>Delete</a>
-        </Space>
-      ),
-    },
-  ];
-  const data = [
-    {
-      key: "1",
-      name: "John Brown",
-      age: 32,
-      address: "New York No. 1 Lake Park",
-      tags: ["nice", "developer"],
-    },
-    {
-      key: "2",
-      name: "Jim Green",
-      age: 42,
-      address: "London No. 1 Lake Park",
-      tags: ["kawaii"],
-    },
-    {
-      key: "3",
-      name: "Joe Black",
-      age: 32,
-      address: "Sydney No. 1 Lake Park",
-      tags: ["cool", "teacher"],
+      render: (_, record) => {
+        return (
+          <Space size="large">
+            <a onClick={() => handleEditUser(record)}>Edit</a>
+            <a
+              onClick={() => handleDeleteUser(record.idUsers)}
+              style={{ color: "red" }}
+            >
+              Delete
+            </a>
+          </Space>
+        );
+      },
     },
   ];
 
-  return <Table columns={columns} dataSource={data} />;
+  const loadUser = async () => {
+    const res = await getAllUserAPI();
+    setDataUser(res.data.data);
+  };
+
+  return <Table columns={columns} dataSource={dataUsers} rowKey="idUsers" />;
 };
+
 export default UserTable;
--- my-app-fontend-2026/src/services/api.service.js
+++ my-app-fontend-2026/src/services/api.service.js
@@ -7,7 +7,13 @@ const createUserAPI = async (data) => {
 };
 
 const updateUserAPI = () => {};
-const deleteUserAPI = () => {};
-const getUserAPI = () => {};
 
-export { createUserAPI, updateUserAPI, deleteUserAPI, getUserAPI };
+const deleteUserAPI = () => {};
+
+const getAllUserAPI = async () => {
+  const URL_BACKEND = "/api/v1/users";
+
+  return await axios.get(URL_BACKEND);
+};
+
+export { createUserAPI, updateUserAPI, deleteUserAPI, getAllUserAPI };
